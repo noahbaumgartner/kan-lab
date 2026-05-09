4fc624434ffe157d65f9387150c1792ceb457b11
noah <baumgartner.noah@outlook.com>
2026-05-09 23:23:55 +0200

    use kanc small

--- src/modules/convkan/kanc.py
+++ src/modules/convkan/kanc.py
@@ -3,45 +3,32 @@ from torch import nn
 from .kanconv import KAN_Convolutional_Layer
 
 
-# KANC_MLP from the paper https://arxiv.org/pdf/2406.13155
 class KANC_MLP(nn.Module):
-    def __init__(
-        self,
-        in_chans: int = 1,
-        img_size: int = 28,
-        num_classes: int = 10,
-        conv1_out: int = 5,
-        conv2_out: int = 10,
-        kernel_size: int = 3,
-        hidden_dim: int = 100,
-        grid_size: int = 5,
-    ):
+    def __init__(self, grid_size: int = 5):
         super().__init__()
         self.conv1 = KAN_Convolutional_Layer(
-            in_channels=in_chans,
-            out_channels=conv1_out,
-            kernel_size=(kernel_size, kernel_size),
-            grid_size=grid_size,
+            in_channels=1, out_channels=5, kernel_size=(3, 3), grid_size=grid_size
         )
+
         self.conv2 = KAN_Convolutional_Layer(
-            in_channels=conv1_out,
-            out_channels=conv2_out,
-            kernel_size=(kernel_size, kernel_size),
-            grid_size=grid_size,
+            in_channels=5, out_channels=5, kernel_size=(3, 3), grid_size=grid_size
         )
-        self.pool = nn.MaxPool2d(kernel_size=(2, 2))
+
+        self.pool1 = nn.MaxPool2d(kernel_size=(2, 2))
+
         self.flat = nn.Flatten()
 
-        s = (img_size - (kernel_size - 1)) // 2
-        s = (s - (kernel_size - 1)) // 2
-        flat_dim = conv2_out * s * s
-
-        self.linear1 = nn.Linear(flat_dim, hidden_dim)
-        self.linear2 = nn.Linear(hidden_dim, num_classes)
+        self.linear1 = nn.Linear(125, 10)
+        self.name = f"KANC MLP (Small) (gs = {grid_size})"
 
     def forward(self, x):
-        x = self.pool(self.conv1(x))
-        x = self.pool(self.conv2(x))
+        x = self.conv1(x)
+
+        x = self.pool1(x)
+
+        x = self.conv2(x)
+        x = self.pool1(x)
         x = self.flat(x)
+        # print(x.shape)
         x = self.linear1(x)
-        return self.linear2(x)
+        return x
