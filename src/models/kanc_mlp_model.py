c532127f8289ec5ea5dd6498f288d6f9aa42027a
noah <baumgartner.noah@outlook.com>
2026-05-09 23:28:54 +0200

    fixed kanc errors

--- src/models/kanc_mlp_model.py
+++ src/models/kanc_mlp_model.py
@@ -5,37 +5,18 @@ from src.modules.convkan import KANC_MLP
 
 
 class KANCMLPModel(BaseKANModel):
-    def __init__(
-        self,
-        num_classes,
-        img_size=28,
-        in_chans=1,
-        conv1_out=5,
-        conv2_out=10,
-        kernel_size=3,
-        hidden_dim=100,
-        grid_size=5,
-        **kwargs,
-    ):
+    def __init__(self, num_classes, img_size=28, in_chans=1, grid_size=5, **kwargs):
         self.num_classes = num_classes
         self.img_size = img_size
         self.in_chans = in_chans
-        self.conv1_out = conv1_out
-        self.conv2_out = conv2_out
-        self.kernel_size = kernel_size
-        self.hidden_dim = hidden_dim
         self.grid_size = grid_size
 
     def build(self, device="cpu"):
         self.model = KANC_MLP(
-            in_chans=self.in_chans,
-            img_size=self.img_size,
-            num_classes=self.num_classes,
-            conv1_out=self.conv1_out,
-            conv2_out=self.conv2_out,
-            kernel_size=self.kernel_size,
-            hidden_dim=self.hidden_dim,
             grid_size=self.grid_size,
+            img_size=self.img_size,
+            in_chans=self.in_chans,
+            num_classes=self.num_classes,
         ).to(device)
         self.device = device
 
